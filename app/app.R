12f3bd83e31b5478afec1c6fc045a782e332ff87
abhimaster2001 <abhimaster2001@gmail.com>
2020-05-02 12:59:51 -0500

    updated app

--- app/app.R
+++ app/app.R
@@ -8,26 +8,42 @@
 #
 
 library(shiny)
+library(lubridate)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("COVID-19 Analysis"),
+    p("By: Abhi Bhattaru"),
+    textOutput("date"),
+    
+    
+    br(),
+    h2("Date retrieval"),
+    p("The EEDC publishes data on COVID19 daily. Files were imported from EEDC.", a(href = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv")),
+    br(),
+    
+    h3("Plot of cummulative cases per day"),
     sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+        sidebarPanel = sidebarPanel(
+            strong("Choose countries"),
+            checkboxGroupInput("countries", "",
+                               choices = c("China", 
+                                           "United_States_of_America",
+                                           "Germany", 
+                                           "Italy",
+                                           "Japan",
+                                           "India")),
+            strong("Choose date range"),
+            dateInput("date", "", 
+                      min=ymd("2020-01-01"),
+                      max=ymd(today()))
+            
+            
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        mainPanel = mainPanel(
+            plotOutput("plotout")
         )
     )
 )
@@ -35,14 +51,28 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    COVID19 <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM")
+    cumalativetable<-COVID19 %>%
+        select(dateRep, cases, geoId, countriesAndTerritories, popData2018)%>%
+        pivot_wider(names_from = dateRep, values_from = cases, values_fill = list(cases=0))%>% #note the list
+        pivot_longer(cols=-c(countriesAndTerritories, geoId, popData2018), names_to="dateRep", values_to='cases')%>%
+        mutate(date=dmy(dateRep))%>%
+        group_by(geoId)%>%
+        arrange(date)%>%
+        mutate(cum_sum = cumsum(cases))%>%
+        mutate(cum_sumperpop = cum_sum*1000000/popData2018)%>%
+        ungroup()
+   
+    output$date <- renderText ({paste('',today())})
+    output$plotout <- renderPlot({
+        plottable<- filter(cumalativetable, countriesAndTerritories %in% input$countries)
+        plot <- ggplot(data=plottable, mapping = aes(date,cum_sum, col=countriesAndTerritories))+
+            geom_line()+
+            geom_point()+
+            geom_hline(yintercept = 10000)
+        plot
     })
+   
 }
 
 # Run the application 
