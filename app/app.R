0d7ce0d93727cf225f477e40b4f4d4edbd711288
abhimaster2001 <abhimaster2001@gmail.com>
2020-07-01 00:09:35 -0500

    fixing up errors

--- app/app.R
+++ app/app.R
@@ -41,7 +41,9 @@ ui <- fluidPage(
         h3("Plot of cummulative cases per day"),
         plotOutput("plotout1"),
         h3("Plot of cases per day"),
-        plotOutput("plotout2")
+        plotOutput("plotout2"),
+        h3("Plot of deaths per day"),
+        plotOutput("plotout3")
     
     
 )
@@ -50,14 +52,14 @@ ui <- fluidPage(
 server <- function(input, output) {
 
     cumalativetable<-COVID19 %>%
-        select(dateRep, cases, geoId, countriesAndTerritories, popData2018)%>%
+        select(dateRep, cases, geoId, countriesAndTerritories, popData2019, deaths)%>%
         pivot_wider(names_from = dateRep, values_from = cases, values_fill = list(cases=0))%>% #note the list
-        pivot_longer(cols=-c(countriesAndTerritories, geoId, popData2018), names_to="dateRep", values_to='cases')%>%
+        pivot_longer(cols=-c(countriesAndTerritories, geoId, popData2019, deaths), names_to="dateRep", values_to='cases')%>%
         mutate(date=dmy(dateRep))%>%
         group_by(geoId)%>%
         arrange(date)%>%
         mutate(cum_sum = cumsum(cases))%>%
-        mutate(cum_sumperpop = cum_sum*1000000/popData2018)%>%
+        mutate(cum_sumperpop = cum_sum*1000000/popData2019)%>%
         ungroup()
    
     output$date <- renderText ({paste('',today())})
@@ -73,14 +75,29 @@ server <- function(input, output) {
         plot
     })
     output$plotout2 <- renderPlot({
-        plottable<- filter(cumalativetable, countriesAndTerritories %in% input$countries,
+        plottable<- cumalativetable %>%
+          filter(countriesAndTerritories %in% input$countries,
                            date >= as_date(input$date[1]) & 
                                date <= as_date(input$date[2]))
-        plot <- ggplot(data=plottable, mapping = aes(date,cases, col=countriesAndTerritories))+
+          
+        plot <- ggplot(data=plottable, mapping = aes(date, cases, col=countriesAndTerritories))+
             labs(x="Date", y="Cases", col = "Countries")+
             theme_classic()+
-            geom_line()+
-            geom_point()
+            geom_point()+
+            geom_line()
+        plot
+    })
+    output$plotout3 <- renderPlot({
+        plottable<- cumalativetable %>%
+            filter(countriesAndTerritories %in% input$countries,
+                   date >= as_date(input$date[1]) & 
+                       date <= as_date(input$date[2]))
+        
+        plot <- ggplot(data=plottable, mapping = aes(date, deaths, col=countriesAndTerritories))+
+            labs(x="Date", y="Deaths", col = "Countries")+
+            theme_classic()+
+            geom_point()+
+            geom_line()
         plot
     })
    
